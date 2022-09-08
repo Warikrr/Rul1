44a6d79246ca6155a8683dd1497867c259f8d1bc
Aeull <110038479+Aeull@users.noreply.github.com>
2022-09-09 01:10:53 +0800

    Update i-donasi.js

--- plugins/i-donasi.js
+++ plugins/i-donasi.js
@@ -1,12 +1,12 @@
 let handler = async (m, { conn, usedPrefix }) => conn.sendButton(m.chat, `
 ╭─「 Donasi • Dana 」
-│ • Tri [0895336282144]
-│ • Gopay  [0895336282144]
-│ • Dana  [0895336282144]
-│ • Saweria  [https://saweria.co/thesadboy01]
+│ • im3 [085822347348]
+│ • axis  [083843192208]
+│ • Dana  [085822347348]
+│ • Saweria  [https://saweria.co/arullofc]
 ╰────
 ╭─「 *NOTE* 」
-│ > Ingin donasi? Wa.me/62895336282144
+│ > Ingin donasi? Wa.me/6285822347348
 │ _Hasil donasi akan digunakan buat sewa_
 │ _atau beli *RDP/VPS* agar bot bisa jalan_
 │ _24jam tanpa kendala_
