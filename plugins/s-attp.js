b95e29a8393a56078ba66f1fc224281ba3ea6ac1
Aeull <110038479+Aeull@users.noreply.github.com>
2022-09-09 16:53:46 +0800

    Update s-attp.js

--- plugins/s-attp.js
+++ plugins/s-attp.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn, text }) => {
 let teks = text ? text : m.quoted && m.quoted.text ? m.quoted.text : 'teksnya mana?'
 /*let sel = global.API('xteam', '/attp', { file: '', text: teks })*/
 let sel = (`https://api.lolhuman.xyz/api/attp2?apikey=SGWN&text=${text}`)
- conn.sendFile(m.chat, sel, 'attp.webp', 0, m, {packname: packname, author: author, mentions: [m.semder]})
+ conn.sendFile(m.chat, sel, 'attp.webp', 0, fake, {packname: packname, author: author, mentions: [m.semder]})
 }
 handler.help = ['attp <teks>']
 handler.tags = ['sticker']
