d1cba5e599ef6a217a56b5a9aadfa9062bbc5138
Aeull <110038479+Aeull@users.noreply.github.com>
2022-09-10 15:03:59 +0800

    Update handler.js

--- handler.js
+++ handler.js
@@ -632,8 +632,8 @@ module.exports = {
                                 namegb: await this.getName(id),
                                 member: groupMetadata.participants.length
                             })
-                            /*await this.send3TemplateButtonImg(id, action === 'add' ? wel : lea, text, wm, action === 'add' ? 'selamat datang', : 'sampai jumpa', action === 'add' ? '.intro', : 'FokusID')*/
-   await conn.sendButtonDoc(id, text, wm, action == 'add' ? 'selamat datang' : 'sampai jumpa', action === 'add' ? '.intro', : 'ArullBotz', fake,{
+                            /*await this.send3TemplateButtonImg(id, action === 'add' ? wel : lea, text, wm, action === 'add' ? 'selamat datang' : 'sampai jumpa', action === 'add' ? '.intro', : 'FokusID')*/
+   await conn.sendButtonDoc(id, text, wm, action == 'add' ? 'selamat datang' : 'sampai jumpa', action === 'add' ? '.intro' : 'ArullBotz', fake,{
   contextInfo: {mentionedJid: [user],
     externalAdReply :{
     showAdAttribution: true,
