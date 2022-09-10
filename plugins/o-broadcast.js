8568884d3a8ad53f39770194daf59bfe05f75a6d
Aeull <110038479+Aeull@users.noreply.github.com>
2022-09-10 18:55:14 +0800

    Update o-broadcast.js

--- plugins/o-broadcast.js
+++ plugins/o-broadcast.js
@@ -4,7 +4,10 @@ let handler  = async (m, { conn, text }) => {
   for (let id of chats) {
        let bcbg = 'https://telegra.ph/file/b2ae8a0e437e4252d5124.png'
        await conn.delay(1500)
-       await conn.send2Button(id, text.trim(), wm, 'Menu', '.menu', 'Owner', '.owner', fake)
+       await conn.send2Button(id, text.trim(), wm, 'Menu', '.menu', 'Owner', '.owner', fake, { contextInfo: {
+      externalAdReply :{
+      showAdAttribution: true,
+      }}})
      }
   m.reply('*Broadcast selesai*')
 }
