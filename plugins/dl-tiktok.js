2dc7f1025e547caaa7de49db8cb4e516346198ed
Aeull <110038479+Aeull@users.noreply.github.com>
2022-09-10 16:08:26 +0800

    Update dl-tiktok.js

--- plugins/dl-tiktok.js
+++ plugins/dl-tiktok.js
@@ -1,28 +1,15 @@
-let fetch = require('node-fetch')
-let axios = require('axios')
-let handler = async (m, { conn, args }) => {
-  if (!args[0]) throw 'Uhm...url nya mana?'
- // let url = `https://api.lolhuman.xyz/api/tiktokwm?apikey=9b817532fadff8fc7cb86862&url=${args[0]}`
+let axios = require('axios');
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+    if (!args[0]) throw `contoh:\n ${usedPrefix}${command} https://www.tiktok.com/@omagadsus/video/7025456384175017243`
     let res = (await axios.get(API('males', '/tiktok', { url: args[0] } ))).data;
-    m.reply(data.wait)
-await conn.reply(m.chat, `Downloading media from Tiktok`, 0, {
-  contextInfo: { mentionedJid: [m.sender],
-    externalAdReply :{
-    showAdAttribution: true,
-    mediaUrl: data.sc,
-    mediaType: 2,
-    description: data.deslink , 
-    title: run,
-    body: wm,
-    thumbnail: await(await fetch(img)).buffer(),
-    sourceUrl: data.sc
-     }}
-  })
-    conn.send2ButtonVid(m.chat, res.video, wm, 'No Wm', '.tiktoknowm ${args[0]}', 'Audio', '.tiktokaudio ${args[0]}', m)
+    if (res.status != 200) throw res.message;
+    if (!res) throw res.message;
+    conn.send2ButtonVid(m.chat, res.video, data.namabot, 'menu', '.menu', 'Owner', '.owner', m)
 }
 handler.help = ['tiktok'].map(v => v + ' <url>')
 handler.tags = ['downloader']
+handler.command = /^(tik(tok)?(dl)?)$/i
 
-handler.command = /^((tt|tiktok)?(dl)?)$/i
+handler.limit = 1
 
 module.exports = handler
