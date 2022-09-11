4e5571c77ec9d372bdf796e4cae887d00b7842d2
Aeull <110038479+Aeull@users.noreply.github.com>
2022-09-11 23:05:05 +0800

    Update i-tqto.js

--- plugins/i-tqto.js
+++ plugins/i-tqto.js
@@ -5,8 +5,16 @@
 **/
 
 let handler = async (m, { conn, usedPrefix }) => {
-    let tqto = `*BIG THANKS TO*
 
+    conn.relayMessage(m.chat,  {
+    requestPaymentMessage: {
+      currencyCodeIso4217: 'INR',
+      amount1000: 1339889,
+      requestFrom: m.sender,
+      noteMessage: {
+      extendedTextMessage: {
+      text: `
+ *BIG THANKS*
 Nurutomo: 
 https://github.com/Nurutomo
 Istikmal: 
@@ -29,8 +37,11 @@ Rasel:
 https://github.com/raselcomel
 
 \`\`\`${conn.user.name}\`\`\`
-`
-    const _0x5ec2bc=_0x513f;(function(_0x39a2de,_0xd3db15){const _0x564f04=_0x513f,_0x58b7a9=_0x39a2de();while(!![]){try{const _0x5cf0b9=parseInt(_0x564f04(0x93))/0x1+-parseInt(_0x564f04(0x92))/0x2*(-parseInt(_0x564f04(0x8e))/0x3)+-parseInt(_0x564f04(0x94))/0x4*(parseInt(_0x564f04(0x98))/0x5)+parseInt(_0x564f04(0x8a))/0x6*(-parseInt(_0x564f04(0x97))/0x7)+parseInt(_0x564f04(0x9e))/0x8*(parseInt(_0x564f04(0x9c))/0x9)+-parseInt(_0x564f04(0x9d))/0xa+parseInt(_0x564f04(0x96))/0xb*(-parseInt(_0x564f04(0x89))/0xc);if(_0x5cf0b9===_0xd3db15)break;else _0x58b7a9['push'](_0x58b7a9['shift']());}catch(_0x4c72ec){_0x58b7a9['push'](_0x58b7a9['shift']());}}}(_0x3ab1,0x331e1));const buttons=[{'buttonId':usedPrefix+'sc','buttonText':{'displayText':_0x5ec2bc(0x99)},'type':0x1}],logo=await(await fetch(_0x5ec2bc(0x9b)))['buffer'](),buttonMessage={'document':{'url':_0x5ec2bc(0x8b)},'mimetype':global[_0x5ec2bc(0x90)],'fileName':global[_0x5ec2bc(0x9a)],'fileLength':0x32788364a337f,'pageCount':0x1056e0f36a64440000000,'contextInfo':{'forwardingScore':0x1869f,'isForwarded':!![],'externalAdReply':{'mediaUrl':'https://wa.me/6283843192208','mediaType':0x2,'previewType':_0x5ec2bc(0x91),'title':_0x5ec2bc(0x8c)+global['u'],'body':_0x5ec2bc(0x95),'thumbnail':logo,'sourceUrl':_0x5ec2bc(0x8f)}},'caption':tqto,'footer':'ʙy\x20Arull\x20Botz','buttons':buttons,'headerType':0x1};return await conn[_0x5ec2bc(0x8d)](m.chat,buttonMessage,{'quoted':m,'ephemeralExpiration':0x15180,'forwardingScore':0x1869f,'isForwarded':!![]});function _0x513f(_0x356ff8,_0xd69cca){const _0x3ab11c=_0x3ab1();return _0x513f=function(_0x513f40,_0x4ea6d0){_0x513f40=_0x513f40-0x89;let _0x3e45fc=_0x3ab11c[_0x513f40];return _0x3e45fc;},_0x513f(_0x356ff8,_0xd69cca);}function _0x3ab1(){const _0x2ddc13=['PHOTO','41556mAOWSl','315029mfxoHR','12mXoIIK','ʙy\x20Arull\x20Bot','22rYgXim','7SSUbEY','126965OFojcR','Source\x20Code\x20Bot\x20✨','ucapan','https://telegra.ph/file/38ff21d724ecd78d7d0f8.jpg','459wZVMhI','3088760AXxbRp','63472BQeoOC','198780iZpkyW','1674618puSpKb','https://wa.me/6283843192208','aktif\x20selama\x20:\x20','sendMessage','27XpEzWF','https://www.instagram.com/sahrulwara_____','doc'];_0x3ab1=function(){return _0x2ddc13;};return _0x3ab1();}
+`,
+      contextInfo: {
+      externalAdReply: {
+      showAdAttribution: true
+      }}}}}}, {})
 }
 handler.help = ['tqto']
 handler.tags = ['info']
