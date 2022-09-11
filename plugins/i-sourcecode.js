81c0b513e8960956b5f6afb28d0ec80c791008a3
Aeull <110038479+Aeull@users.noreply.github.com>
2022-09-11 22:28:44 +0800

    Update i-sourcecode.js

--- plugins/i-sourcecode.js
+++ plugins/i-sourcecode.js
@@ -1,26 +1,18 @@
 let handler = async (m, { conn }) => {
-	
-	// Link Script jgn di ganti ya pliss
-    // Kalo mau masukin github kamu tinggal add aja link githubnya tapi jangan di hapus sumber script!
-    // Jangan Ngeyell..
-    // Kalo ngeyel gk bakal gw up sc lgi.
-    
-    let txt = `
-*NIH:*
+
+     conn.relayMessage(m.chat,  {
+    requestPaymentMessage: {
+      currencyCodeIso4217: 'INR',
+      amount1000: 1339889,
+      requestFrom: m.sender,
+      noteMessage: {
+      extendedTextMessage: {
+      text: '*NIH:*
 *https://youtu.be/IL2PZql859c*
 *Jangan Lupa Subscribe*
 
 *My Github:*
-*https://github.com/Warikrr*
-
-`
-     conn.relayMessage(m.chat,  {
-    requestPaymentMessage: {
-      currencyCodeIso4217: 'RP',
-      amount1000: 2000000000000000000,
-      requestFrom: m.sender,
-      noteMessage: {
-      extendedTextMessage: {
+*https://github.com/Warikrr*',
       contextInfo: {
       externalAdReply: {
       showAdAttribution: true
