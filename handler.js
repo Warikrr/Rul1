4aece0dca5b84a23609288872ccdb77909373db5
Aeull <110038479+Aeull@users.noreply.github.com>
2022-09-13 19:56:57 +0800

    Update handler.js

--- handler.js
+++ handler.js
@@ -617,13 +617,13 @@ module.exports = {
                         } finally {
                             text = (action === 'add' ? (chat.sWelcome || this.welcome || conn.welcome || 'Yah,si Beban Masuk Grup @user').replace('@subject', groupMetadata.subject).replace('@desc', groupMetadata.desc.toString()) :
                                 (chat.sBye || this.bye || conn.bye || 'Sip, Beban Berkurang @user!')).replace('@user', '@' + user.split('@')[0])
-                                this.reply(id, pp, text, fake, { contextInfo: { externalAdReply :{
+                                this.send2ButtonImg(id, pp, text, "𝗔𝗿𝘂𝗹𝗹𝗕𝗼𝘁𝘇", "Script Bot", ".sc", "Tampilan Menu", ".menu", fake, { contextInfo: { externalAdReply :{
                                 showAdAttribution: true,
-                                mediaUrl: data.linkgc,
+                                mediaUrl: data.sc,
                                 mediaType: 2,
-                                sourceUrl: data.linkgc,
+                                sourceUrl: data.sc,
                                 body: data.namabot,
-                                thumbnail: await(await fetch (pp)).buffer(),
+                                thumbnail: await(await fetch (img)).buffer(),
                                 title: action === 'add' ? 'Semoga Betah Yah Kak🤗' : 'Yah Kok Keluar Sih😩',
                                 }}})
                                 }
