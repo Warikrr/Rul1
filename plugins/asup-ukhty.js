9b5b08afe95dfc7f3b4ef81b446c9988de4c92ed
Aeull <110038479+Aeull@users.noreply.github.com>
2022-09-13 21:49:21 +0800

    Update asup-ukhty.js

--- plugins/asup-ukhty.js
+++ plugins/asup-ukhty.js
@@ -9,6 +9,7 @@ let handler = async(m, { conn, usedPrefix, command }) => {
 }
 handler.help = ['ukhty']
 handler.tags = ['asupan']
+handler.premium = true,
 handler.command = /^(ukhty)$/i
 //MADE IN ERPAN 1140 BERKOLABORASI DENGAN BTS
 module.exports = handler
