0efd46e4f4c56d7f70542b901e51cefb7c5160d1
Aeull <110038479+Aeull@users.noreply.github.com>
2022-09-13 12:01:02 +0800

    Create main-menu1.js

---
+++ plugins/main-menu1.js
@@ -0,0 +1,13 @@
+let handler = async (m, { conn, usedPrefix: _p, args, command }) => {
+let moment = require('moment-timezone')
+
+conn.send2ButtonDoc(m.chat, 'ʜᴀɪ,ᴀᴋᴜ ᴀᴅᴀʟᴀʜ ʙᴏᴛ ʏᴀɴɢ ᴅɪʙᴇʀɪ ɴᴀᴍᴀ ArullBotz,ᴀᴋᴜ ᴅɪ ᴋᴇᴍʙᴀɴɢᴋᴀɴ ᴏʟᴇʜ ꜱᴇꜱᴇᴏʀᴀɴɢ ʏᴀɴɢ ʙᴇʀɴᴀᴍᴀ Sahrull,ʙᴇʟɪᴀᴜ ᴍᴀꜱɪʜ ᴘᴇᴍᴜʟᴀ ᴅᴀɴ ᴍᴀꜱɪʜ ʙᴇʟᴀᴊᴀʀ ʙᴀɴʏᴀᴋ ʜᴀʟ,ᴅᴀɴ ᴛᴜᴊᴜᴀɴɴʏᴀ ʜᴀɴʏᴀ ɪɴɢɪɴ ᴛᴀᴜ ʙᴇʀʙᴀɢᴀɪ ʜᴀʟ ʏᴀɴɢ ᴀᴅᴀ ᴅɪᴅᴜɴɪᴀ ʙᴏᴛ', data.namabot, 'ʟɪꜱᴛᴍᴇɴᴜ', '.m', 'ᴏᴡɴᴇʀ', '.owner', fake)
+}
+
+handler.help = ['menu']
+handler.tags = ['main']
+handler.command = /^(menu)$/i
+
+module.exports = handler
+
+
