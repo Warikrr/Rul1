678c1a35e612391557ba5de0a03232805799a292
Aeull <110038479+Aeull@users.noreply.github.com>
2022-09-13 10:13:25 +0800

    Update afk.js

--- plugins/afk.js
+++ plugins/afk.js
@@ -2,9 +2,10 @@ let handler = async(m, { conn, usedPrefix, text }) => {
     let user = global.db.data.users[m.sender]
     user.afk = + new Date
     user.afkReason = text
-    conn.sendButtonDoc(m.chat, `
+    conn.sendButton(m.chat, `
 ${conn.getName(m.sender)} sekarang AFK${text ? ': ' + text : ''}
-`, 'Oke', 'ok', wm, m)
+`, wm, 'Okee', 'p', fake, { contextInfo: { externalAdReply :{ showAdAttribution: true,
+}}})
 }
 handler.help = ['afk <reason>']
 handler.tags = ['main']
