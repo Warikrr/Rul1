c8c3c87f538d41b7611940f52beabefc6b8c2679
Diluc <91557468+Dilucal@users.noreply.github.com>
2022-09-20 01:25:51 +0700

    Update main-menu1.js

--- plugins/main-menu1.js
+++ plugins/main-menu1.js
@@ -3,7 +3,7 @@ let moment = require('moment-timezone')
 let time = moment.tz('Asia/Jakarta').format("HH:mm:ss")
 let date = moment.tz('Asia/Jakarta').format("dddd, Do MMMM, YYYY")
 
-conn.sendButtonDoc(m.chat, 'Hɪɪ, Iᴍ ᴀ ᴡʜᴀᴛsᴀᴘᴘ ʀᴏʙᴏᴛ ɴᴀᴍᴇᴅ Qʏɴᴢᴜᴜ. ᴀɴ ᴀᴜᴛᴏᴍᴀᴛᴇᴅ sʏsᴛᴇᴍ (ᴡʜᴀᴛsAᴘᴘ Bᴏᴛ) ᴛʜᴀᴛ ᴄᴀɴ ʜᴇʟᴘ ᴛᴏ ᴅᴏ sᴏᴍᴇᴛʜɪɴɢ, sᴇᴀʀᴄʜ ᴀɴᴅ ɢᴇᴛ ᴅᴀᴛᴀ / ɪɴғᴏʀᴍᴀᴛɪᴏɴ ᴏɴʟʏ ᴛʜʀᴏᴜɢʜ WʜᴀᴛsAᴘᴘ,Tᴏ sᴇᴇ ᴛʜᴇ ʙᴏᴛ ғᴇᴀᴛᴜʀᴇs ᴄʟɪᴄᴋ ᴛʜᴇ ʙᴜᴛᴛᴏɴ ʙᴇʟᴏᴡ\n\nɴᴏᴛᴇ: ᴘʟᴇᴀsᴇ ᴜsᴇ ʙᴏᴛs ᴡɪsᴇʟʏ ᴀɴᴅ ᴅᴏɴ'ᴛ sᴘᴀᴍᴍ!!', data.namabot, 'ʟɪꜱᴛᴍᴇɴᴜ', '.m', fake, { contextInfo: {
+conn.sendButtonDoc(m.chat, 'Hɪɪ, Iᴍ ᴀ ᴡʜᴀᴛsᴀᴘᴘ ʀᴏʙᴏᴛ ɴᴀᴍᴇᴅ Qʏɴᴢᴜᴜ. ᴀɴ ᴀᴜᴛᴏᴍᴀᴛᴇᴅ sʏsᴛᴇᴍ (ᴡʜᴀᴛsAᴘᴘ Bᴏᴛ) ᴛʜᴀᴛ ᴄᴀɴ ʜᴇʟᴘ ᴛᴏ ᴅᴏ sᴏᴍᴇᴛʜɪɴɢ, sᴇᴀʀᴄʜ ᴀɴᴅ ɢᴇᴛ ᴅᴀᴛᴀ / ɪɴғᴏʀᴍᴀᴛɪᴏɴ ᴏɴʟʏ ᴛʜʀᴏᴜɢʜ WʜᴀᴛsAᴘᴘ,Tᴏ sᴇᴇ ᴛʜᴇ ʙᴏᴛ ғᴇᴀᴛᴜʀᴇs ᴄʟɪᴄᴋ ᴛʜᴇ ʙᴜᴛᴛᴏɴ ʙᴇʟᴏᴡ\n\nɴᴏᴛᴇ: ᴘʟᴇᴀsᴇ ᴜsᴇ ʙᴏᴛs ᴡɪsᴇʟʏ ᴀɴᴅ ᴅᴏɴᴛ sᴘᴀᴍᴍ!!', data.namabot, 'ʟɪꜱᴛᴍᴇɴᴜ', '.m', fake, { contextInfo: {
 externalAdReply :{
     showAdAttribution: true,
     mediaUrl: data.sc,
